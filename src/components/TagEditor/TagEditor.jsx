dc5d3984a2b0e785dc587fa6fd2b6a3ce60bd5fb
David Wilson <w.dave.w@gmail.com>
2017-01-10 13:40:44 -0800

    initial redux directory setup

--- src/components/TagEditor/TagEditor.jsx
+++ src/components/TagEditor/TagEditor.jsx
@@ -11,15 +11,6 @@ class TagEditor extends Component {
     return (
       <div className="tag-editor">
         <h5 className="tag-editor-title">TAGS</h5>
-        <span
-          style={{color: '#d5dadf', fontSize: '12px', paddingTop: '15px'}}
-          /* you can remove this span */
-        >
-          /*
-            The rest of the code shoud be placed here and is up to you.
-            This is a starting point to let your ideas run free.
-          */
-        </span>
       </div>
     );
   }
