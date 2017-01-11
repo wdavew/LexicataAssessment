0c32ad2e179c6ca055f27ba8a86cc972af554d5f
David Wilson <w.dave.w@gmail.com>
2017-01-10 23:33:08 -0800

    connect tagEditor container to redux store

--- src/components/TagEditor/TagEditor.jsx
+++ src/components/TagEditor/TagEditor.jsx
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actionCreators from '../../actionCreators.js';
+import TagIcon from './TagIcon';
+import Dropdown from './Dropdown';
+import ColorSelect from './ColorSelect';
+import ClickableButton from './ClickableButton';
+import Input from './Input';
+
+
 // An array of existing tags. Just uncomment when you want to use them.
 // import tagsJSON from '../../api/tags.json';
 import './TagEditor.css';
 
-
 class TagEditor extends Component {
-
   render() {
+    const {
+      displayedTags,
+      availableTags,
+      tagName,
+      removeTag,
+      changeName,
+      submitTagName,
+      submitTag,
+      selectTagColor,
+      displayTagSelect,
+      displayTag,
+      colorSelectVisible,
+      dropDownVisible,
+    } = this.props;
+    const tagList = displayedTags.map((tag) => (
+      <TagIcon key={tag.label} label={tag.label} color={tag.color} removeSelf={removeTag} />
+    ));
     return (
       <div className="tag-editor">
         <h5 className="tag-editor-title">TAGS</h5>
+        <div id='tag-display'>
+          {tagList}
+        </div>
+
+        <div id='tag-creation'>
+          <div id='input-container'>
+            <Input value={tagName} handleChange={changeName} dropdown={displayTagSelect} />
+            <Dropdown availableTags={availableTags} visible={dropDownVisible} addTag={displayTag} />
+          </div>
+          <ClickableButton label="NEW" idName={"color-select-btn"} handleClick={submitTagName} />
+          <ClickableButton label="Add Tag" idName={"submit-tag-btn"} handleClick={submitTag} />
+        </div>
+        <ColorSelect selectColor={selectTagColor} visible={colorSelectVisible} />
       </div>
     );
   }
 }
 
-export default TagEditor;
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps, actionCreators)(TagEditor);
