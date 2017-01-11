2079a9d494d2f86f5c5de27e36c11d251e6ed959
David Wilson <w.dave.w@gmail.com>
2017-01-10 23:34:11 -0800

    add dropdown presentation component

---
+++ src/components/TagEditor/Dropdown.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default ({availableTags, visible, addTag}) => {
+  const visibility = visible ? 'block' : 'none';
+
+  const tags = availableTags.map((tag) => {
+    const squareStyle = { backgroundColor: tag.color };
+    return (
+      <li key={tag.label} className="tag-select" onMouseDown={() => addTag(tag.label)}>
+        <div className="color-square" style={squareStyle}></div>
+        <span className='tag-display-label' >{tag.label}</span>
+      </li>
+    );
+  });
+  return (
+    <ul id="tag-dropdown" style={{ "display": visibility }} >
+      {tags}
+    </ul>
+  );
+};
